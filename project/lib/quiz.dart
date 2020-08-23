9ff59e84e1f755c524f04203a0bf33e75eeba2d1
AksV73 <michikoaks@gmail.com>
2020-08-23 18:03:50 +0530

    Scroll added for first two

--- project/lib/quiz.dart
+++ project/lib/quiz.dart
@@ -41,108 +41,110 @@ class qu1 extends StatelessWidget {
             ),
           ],
         ),
-        body: Container(
-          child: Card(
-            child: Container(
-              padding: EdgeInsets.all(10.0),
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.all(15.0),
-                    height: 70.0,
-                    width: devwidth - 10.0,
-                    color: Colors.white24,
-                    child: Text(
-                      'Question 1',
-                      style: TextStyle(
-                        fontSize: 30.0,
-                        fontFamily: 'DancingScript',
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-
-                  Container(
-                    padding: EdgeInsets.fromLTRB(20.0, 30.0, 20.0, 30.0),
-                    height: 300.0,
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        colors: _colors,
-                      ),
-                    ),
-                    child: Column(
-                      children: <Widget>[
-                        Container(
-                          padding: EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 20.0),
-                          child: Text(
-                            "What kind of accommodation would you provide your dog ?",
-                            style: TextStyle( fontSize: 20.0, fontFamily: 'Roboto', fontStyle: FontStyle.italic),
-                          ),
+        body: SingleChildScrollView(
+          child: Container(
+            child: Card(
+              child: Container(
+                padding: EdgeInsets.all(10.0),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.all(15.0),
+                      height: 70.0,
+                      width: devwidth - 10.0,
+                      color: Colors.white24,
+                      child: Text(
+                        'Question 1',
+                        style: TextStyle(
+                          fontSize: 30.0,
+                          fontFamily: 'DancingScript',
                         ),
-                        RaisedButton(
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+
+                    Container(
+                      padding: EdgeInsets.fromLTRB(20.0, 30.0, 20.0, 30.0),
+                      height: 300.0,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: _colors,
+                        ),
+                      ),
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 20.0),
+                            child: Text(
+                              "What kind of accommodation would you provide your dog ?",
+                              style: TextStyle( fontSize: 20.0, fontFamily: 'Roboto', fontStyle: FontStyle.italic),
+                            ),
+                          ),
+                          RaisedButton(
+                              onPressed: (){
+                                temp = "apartment ";
+                              },
+                              highlightColor: Colors.green,
+                              child: Text("An apartment",
+                                style: TextStyle( fontFamily: 'Roboto'),
+                              )),
+                          RaisedButton(
                             onPressed: (){
-                              temp = "apartment ";
+                              temp = "independent house";
                             },
                             highlightColor: Colors.green,
-                            child: Text("An apartment",
+                            child: Text("An Independent house",
                               style: TextStyle( fontFamily: 'Roboto'),
-                            )),
-                        RaisedButton(
-                          onPressed: (){
-                            temp = "independent house";
-                          },
-                          highlightColor: Colors.green,
-                          child: Text("An Independent house",
-                            style: TextStyle( fontFamily: 'Roboto'),
+                            ),
                           ),
-                        ),
-                        RaisedButton(
-                          onPressed: (){ temp = "ranch"; },
-                          highlightColor: Colors.green,
-                          child: Text("A farmhouse/ranch",
-                            style: TextStyle( fontFamily: 'Roboto'),
+                          RaisedButton(
+                            onPressed: (){ temp = "ranch"; },
+                            highlightColor: Colors.green,
+                            child: Text("A farmhouse/ranch",
+                              style: TextStyle( fontFamily: 'Roboto'),
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    height: 50.0,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: RaisedButton(
-                      elevation: 60.0 ,
-                      textColor: Colors.white,
-                      color: Colors.black54,
-                      highlightColor: Colors.red,
-                      child: Text(
-                        'Next',
-                        style: TextStyle(
-                            fontSize: 20.0,
-                            fontFamily: 'DancingScript'
-                        ),
-                      ),
-                      onPressed: () {
-                        addData(temp);
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => qu2()));
-                      },
-                    ),
-                  ),
-                  Container(
-                    height: 100.0,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage('images/paw-trail.jpg'),
-                        fit: BoxFit.fill,
+                        ],
                       ),
                     ),
-                  ),
-                  Text(
-                    'Partners in pethood',
-                    style: TextStyle( fontFamily: 'Courgette'),
-                  )
-                ],
+                    Container(
+                      height: 50.0,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: RaisedButton(
+                        elevation: 60.0 ,
+                        textColor: Colors.white,
+                        color: Colors.black54,
+                        highlightColor: Colors.red,
+                        child: Text(
+                          'Next',
+                          style: TextStyle(
+                              fontSize: 20.0,
+                              fontFamily: 'DancingScript'
+                          ),
+                        ),
+                        onPressed: () {
+                          addData(temp);
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => qu2()));
+                        },
+                      ),
+                    ),
+                    Container(
+                      height: 100.0,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('images/paw-trail.jpg'),
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      'Partners in pethood',
+                      style: TextStyle( fontFamily: 'Courgette'),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
@@ -189,113 +191,121 @@ class qu2 extends StatelessWidget {
             ),
           ],
         ),
-        body: Container(
-          child: Card(
-            child: Container(
-              padding: EdgeInsets.all(10.0),
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.all(15.0),
-                    height: 70.0,
-                    width: devwidth - 10.0,
-                    color: Colors.white24,
-                    child: Text(
-                      'Question 2',
-                      style: TextStyle(
-                        fontSize: 30.0,
-                        fontFamily: 'DancingScript',
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-
-                  Container(
-                    padding: EdgeInsets.fromLTRB(20.0, 30.0, 20.0, 30.0),
-                    height: 300.0,
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        colors: _colors,
-                      ),
-                    ),
-                    child: Column(
-                      children: <Widget>[
-                        Container(
-                          padding: EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 20.0),
-                          child: Text(
-                            "How much barking can you tolerate ?",
-                            style: TextStyle( fontSize: 20.0, fontFamily: 'Roboto', fontStyle: FontStyle.italic),
-                          ),
+        body: SingleChildScrollView(
+          child: Container(
+            child: Card(
+              child: Container(
+                padding: EdgeInsets.all(10.0),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.all(15.0),
+                      height: 70.0,
+                      width: devwidth - 10.0,
+                      color: Colors.white24,
+                      child: Text(
+                        'Question 2',
+                        style: TextStyle(
+                          fontSize: 30.0,
+                          fontFamily: 'DancingScript',
                         ),
-                        RaisedButton(
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+
+                    Container(
+                      padding: EdgeInsets.fromLTRB(20.0, 30.0, 20.0, 30.0),
+                      height: 300.0,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: _colors,
+                        ),
+                      ),
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 20.0),
+                            child: Text(
+                              "How much barking can you tolerate ?",
+                              style: TextStyle( fontSize: 20.0, fontFamily: 'Roboto', fontStyle: FontStyle.italic),
+                            ),
+                          ),
+                          RaisedButton(
+                              onPressed: (){
+                                temp = "high ";
+                              },
+                              highlightColor: Colors.green,
+                              child: Text("Enough to blow the roof off!",
+                                style: TextStyle( fontFamily: 'Roboto'),
+                              )),
+                          RaisedButton(
                             onPressed: (){
-                              temp = "high ";
+                              temp = "low";
                             },
                             highlightColor: Colors.green,
-                            child: Text("Enough to blow the roof off!",
+                            child: Text("A woof once in a while",
                               style: TextStyle( fontFamily: 'Roboto'),
-                            )),
-                        RaisedButton(
-                          onPressed: (){
-                            temp = "low";
-                          },
-                          highlightColor: Colors.green,
-                          child: Text("A woof once in a while",
-                            style: TextStyle( fontFamily: 'Roboto'),
+                            ),
                           ),
-                        ),
-                        RaisedButton(
-                          onPressed: (){ temp = "moderate"; },
-                          highlightColor: Colors.green,
-                          child: Text("Somewhere in between the two",
-                            style: TextStyle( fontFamily: 'Roboto'),
+                          RaisedButton(
+                            onPressed: (){ temp = "moderate"; },
+                            highlightColor: Colors.green,
+                            child: Text("Somewhere in between the two",
+                              style: TextStyle( fontFamily: 'Roboto'),
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    height: 50.0,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: RaisedButton(
-                      elevation: 60.0 ,
-                      textColor: Colors.white,
-                      color: Colors.black54,
-                      highlightColor: Colors.red,
-                      child: Text(
-                        'Next',
-                        style: TextStyle(
-                            fontSize: 20.0,
-                            fontFamily: 'DancingScript'
-                        ),
-                      ),
-                      onPressed: () {
-                        addData(temp);
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => qu3()));
-                      },
-                    ),
-                  ),
-                  Container(
-                    height: 100.0,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage('images/paw-trail.jpg'),
-                        fit: BoxFit.fill,
+                        ],
                       ),
                     ),
-                  ),
-                  Text(
-                    'Partners in pethood',
-                    style: TextStyle( fontFamily: 'Courgette'),
-                  )
-                ],
+                    Container(
+                      height: 50.0,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: RaisedButton(
+                        elevation: 60.0 ,
+                        textColor: Colors.white,
+                        color: Colors.black54,
+                        highlightColor: Colors.red,
+                        child: Text(
+                          'Next',
+                          style: TextStyle(
+                              fontSize: 20.0,
+                              fontFamily: 'DancingScript'
+                          ),
+                        ),
+                        onPressed: () {
+                          addData(temp);
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => qu3()));
+                        },
+                      ),
+                    ),
+
+                       Container(
+                        height: 100.0,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('images/paw-trail.jpg'),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
+
+
+                       Text(
+                        'Partners in pethood',
+                        style: TextStyle( fontFamily: 'Courgette'),
+                      ),
+
+                    ]
+
+                )
+                ),
               ),
             ),
-          ),
-        )
-    );
+        ),
+        );
+
   }
 }
 
